90995836c6cf97f70682ad1a291af9c06fc608db
caishanglin <shang_cai@appdet.com>
2022-02-15 16:51:04 +0800

    2022/02/15 1.設定完成

---
+++ app/src/main/java/com/shang/jetpackmovie/dialog/DisclaimerDialog.kt
@@ -0,0 +1,21 @@
+package com.shang.jetpackmovie.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.WindowManager
+import com.shang.jetpackmovie.R
+
+class DisclaimerDialog (context: Context) : Dialog(context, R.style.FullScreenDialog) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_disclaimer)
+
+        val layoutParams: WindowManager.LayoutParams? = window?.attributes
+        layoutParams?.width = ViewGroup.LayoutParams.MATCH_PARENT
+        layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
+        window?.attributes = layoutParams
+//        window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+    }
+}
--- app/src/main/java/com/shang/jetpackmovie/fragment/setting/ui/DisclaimerModel.kt
+++ app/src/main/java/com/shang/jetpackmovie/fragment/setting/ui/DisclaimerModel.kt
@@ -6,11 +6,19 @@ import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.shang.jetpackmovie.R
+import com.shang.jetpackmovie.dialog.DisclaimerDialog
 
 @EpoxyModelClass(layout = R.layout.epoxy_setting_disclaimer)
-abstract class DisclaimerModel : EpoxyModelWithHolder<DisclaimerModel.DisclaimerViewHolder>(){
+abstract class DisclaimerModel : EpoxyModelWithHolder<DisclaimerModel.DisclaimerViewHolder>() {
 
-    class DisclaimerViewHolder: EpoxyHolder(){
+    override fun bind(holder: DisclaimerViewHolder) {
+        super.bind(holder)
+        holder.vContent.setOnClickListener {
+            DisclaimerDialog(it.context).show()
+        }
+    }
+
+    class DisclaimerViewHolder : EpoxyHolder() {
         lateinit var vContent: ConstraintLayout
         override fun bindView(itemView: View) {
             vContent = itemView.findViewById(R.id.vContent)
