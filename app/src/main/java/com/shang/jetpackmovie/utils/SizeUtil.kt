50a5415413784c6fe473c1778604802e922dd84f
caishanglin <shang_cai@appdet.com>
2022-03-08 16:43:04 +0800

    2022/3/8 1.新增dp

--- app/src/main/java/com/shang/jetpackmovie/utils/SizeUtil.kt
+++ app/src/main/java/com/shang/jetpackmovie/utils/SizeUtil.kt
@@ -1,5 +1,8 @@
 package com.shang.jetpackmovie.utils
 
 import android.content.Context
+import com.shang.jetpackmovie.core.JetpackMovieApplication
 
 fun Int.dp2px(context: Context): Int = (this * context.resources.displayMetrics.density).toInt()
+
+fun Int.dp(): Int = (this * JetpackMovieApplication.context!!.resources.displayMetrics.density).toInt()
