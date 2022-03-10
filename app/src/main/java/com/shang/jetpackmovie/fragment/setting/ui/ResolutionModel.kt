4d073e8d48a243bbcbf6b936a141752f271a7925
caishanglin <shang_cai@appdet.com>
2022-03-10 14:58:30 +0800

    2022/3/9 1.LoadMore

--- app/src/main/java/com/shang/jetpackmovie/fragment/setting/ui/ResolutionModel.kt
+++ app/src/main/java/com/shang/jetpackmovie/fragment/setting/ui/ResolutionModel.kt
@@ -6,6 +6,7 @@ import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.shang.jetpackmovie.R
+import com.shang.jetpackmovie.activity.children.ChildrenActivity
 
 @EpoxyModelClass(layout = R.layout.epoxy_setting_resolution)
 abstract class ResolutionModel : EpoxyModelWithHolder<ResolutionModel.ResolutionViewHolder>(){
@@ -14,6 +15,9 @@ abstract class ResolutionModel : EpoxyModelWithHolder<ResolutionModel.Resolution
         lateinit var vContent: ConstraintLayout
         override fun bindView(itemView: View) {
             vContent = itemView.findViewById(R.id.vContent)
+            vContent.setOnClickListener{
+                ChildrenActivity.start(it.context,634649)
+            }
         }
     }
 }
